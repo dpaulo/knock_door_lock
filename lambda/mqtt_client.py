8d43e8bbb359c2291f7953102a39a5d57ec08f1c
Dovydas Paulauskas <47575688+dpaulo@users.noreply.github.com>
2022-05-16 15:29:04 +0100

    Update mqtt_client.py

--- lambda/mqtt_client.py
+++ lambda/mqtt_client.py
@@ -2,11 +2,15 @@ import boto3
 import os
 import json
 
+# load the environmental variables
 host = os.environ.get('AWS_IOT_ENDPOINT')
 port = os.environ.get('AWS_IOT_PORT')
 thingName = os.environ.get('AWS_IOT_THINGNAME')
+
+# initialize the Boto3 MQTT client for IoT data transmission, with the IoT Thing region specified
 mqttClient = boto3.client('iot-data', 'eu-west-1')
 
+# function to load the payload to unlock the door
 def unlock_door():
     
     payload_dict = {
@@ -19,6 +23,7 @@ def unlock_door():
     
     publish_update(payload_dict)
     
+# function to load the payload to lock the door
 def lock_door():
     
     payload_dict = {
@@ -30,8 +35,8 @@ def lock_door():
     }
     
     publish_update(payload_dict)
-    
+
+# function to publish the payload to the IoT Thing shadow topic
 def publish_update(payload_dict):
     JSON_payload = json.dumps(payload_dict)
-    response = mqttClient.update_thing_shadow(thingName=thingName, payload=JSON_payload)
-    print(json.loads(response['payload'].read()))
+    mqttClient.update_thing_shadow(thingName=thingName, payload=JSON_payload)
